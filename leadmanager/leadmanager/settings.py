ee7e872c42a005fe68b7829bae212acbaab3592d
Matthew Eldredge <mattheweldredge@gmail.com>
2021-05-05 16:47:03 -0700

    feat(main): install babel, react, webpack, add views

--- leadmanager/leadmanager/settings.py
+++ leadmanager/leadmanager/settings.py
@@ -26,7 +26,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'leads',
-    'rest_framework'
+    'rest_framework',
+    'frontend'
 ]
 
 MIDDLEWARE = [
