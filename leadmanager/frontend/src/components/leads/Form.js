6ea610cf60770bce8cb7fdb496d7f1a298933074
Matthew Eldredge <mattheweldredge@gmail.com>
2021-05-05 18:22:28 -0700

    feat(main): start init layout

---
+++ leadmanager/frontend/src/components/leads/Form.js
@@ -0,0 +1,9 @@
+import React, { Fragment } from 'react';
+
+export default function Form() {
+  return (
+    <Fragment>
+      <h1>Add Lead Form</h1>
+    </Fragment>
+  );
+};
---
+++ leadmanager/frontend/src/components/leads/Leads.js
@@ -0,0 +1,9 @@
+import React, { Fragment } from 'react';
+
+export default function Leads() {
+  return (
+    <Fragment>
+      <h1>Leads List</h1>
+    </Fragment>
+  );
+};
